87fdabbb872d927821d5c41ecec624ce559caf23
zhangdf <define.zhang@qq.com>
2019-01-23 15:25:55 +0800

    添加启动闪屏splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:day1stopwatch/pages/home.dart';
+// import 'package:day1stopwatch/pages/home.dart';
+import 'package:day1stopwatch/splash_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: SplashScreen(),
     );
   }
 }
